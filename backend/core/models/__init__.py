202d4af47f13537ec321f36b7348f36e9c525667
Pavlo Kozovyi <p.kozoviy@gmail.com>
2025-11-02 16:17:11 +0200

    feat(db): alembic integration

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,4 +1,6 @@
 from core.models.base import Base
 from core.models.user import User
+from core.models.token import AccessToken
 
-__all__ = ("Base","User")
+
+__all__ = ("Base","User","AccessToken")
