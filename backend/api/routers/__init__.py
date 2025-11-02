92ab6e57979d09c96f658a707cb80c18e0a65c94
Pavlo Kozovyi <p.kozoviy@gmail.com>
2025-11-02 16:26:34 +0200

    feat(auth): add http_bearer dep

--- backend/api/routers/__init__.py
+++ backend/api/routers/__init__.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 from api.routers.user import router as user_router
 from api.routers.auth import router as auth_router
 from api.routers.chat import router as chat_router
 
-router = APIRouter(prefix="/v1")
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(prefix="/v1", dependencies=[Depends(http_bearer)])
 router.include_router(user_router, prefix="/user", tags=["User"])
 router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 router.include_router(chat_router, prefix="/chat", tags=["Chat"])
